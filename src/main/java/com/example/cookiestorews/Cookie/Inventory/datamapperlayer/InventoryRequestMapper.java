95a152f67b5bb6ed76dd6a6ba8eb10d752f64ede
VladLoghin <blade.loghin@gmail.com>
2023-03-04 18:29:50 -0500

    working on fixing InventoryRequestMapper

--- src/main/java/com/example/cookiestorews/Cookie/Inventory/datamapperlayer/InventoryRequestMapper.java
+++ src/main/java/com/example/cookiestorews/Cookie/Inventory/datamapperlayer/InventoryRequestMapper.java
@@ -4,10 +4,14 @@ import com.example.cookiestorews.Cookie.Inventory.datalayer.Inventory;
 import com.example.cookiestorews.Cookie.Inventory.presentationlayer.InventoryRequestModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface InventoryRequestMapper {
-    @Mapping(target="id", ignore = true)
-    @Mapping(target = "inventoryIdentifier", ignore = true)
-    Inventory requestModelToEntity(InventoryRequestModel requestModel);
+    @Mappings ({
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "inventoryIdentifier", ignore = true)
+    })
+    Inventory requestModelToEntity (InventoryRequestModel requestModel);
+
 }
